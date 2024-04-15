64a2c143544fe019fe829397b673380a87c78fc6
ElenaUM <luzunova99@gmail.com>
2024-04-15 22:22:49 +0300

    додано task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,4 +1,31 @@
-function getShippingCost(country) {}
+function getShippingCost(country) {
+  const priceToAustralia = 170;
+  const priceToChina = 100;
+  const priceToChile = 250;
+  const priceToJamaica = 120;
+  let message = ``;
+
+  switch (country) {
+    case "Australia":
+      price = 170;
+      return (message = `Shipping to ${country} will cost ${price} credits`);
+
+    case "China":
+      price = 100;
+      return (message = `Shipping to ${country} will cost ${price} credits`);
+
+    case "Chile":
+      price = 250;
+      return (message = `Shipping to ${country} will cost ${price} credits`);
+
+    case "Jamaica":
+      price = 120;
+      return (message = `Shipping to ${country} will cost ${price} credits`);
+
+    default:
+      return (message = `Sorry, there is no delivery to your country`);
+  }
+}
 
 console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
 console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
